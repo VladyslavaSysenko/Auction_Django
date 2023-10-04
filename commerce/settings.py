fd3923fbda9b84e3afc9f2e0f6261338e8bbe2e9
Vladyslava Sysenko <vlada.sysenko.2002@gmail.com>
2023-10-04 16:47:06 +0300

    change static setting

--- commerce/settings.py
+++ commerce/settings.py
@@ -108,4 +108,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = ""
 STATIC_URL = "/static/"
+STATICFILES_DIRS = ("static",)
